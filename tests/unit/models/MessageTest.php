d08807456a965357ed04cc8967042362845d7087
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2024-03-09 19:35:36 +0400

    [MSTMS-2] covered init message.

--- tests/unit/models/MessageTest.php
+++ tests/unit/models/MessageTest.php
@@ -9,4 +9,16 @@ use PHPUnit\Framework\TestCase;
  */
 class MessageTest extends TestCase
 {
+    /**
+     * @dataProvider getInitMessageProvidedData
+     */
+    public function testInitMessage(
+        array $messageData,
+        array $expectedMessageData
+    ): void {
+        $this->assertActualMessageDataEqualsExpected(
+            (new Message($messageData['text'], $messageData['title'])),
+            $expectedMessageData
+        );
+    }
 }
