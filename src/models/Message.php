4237651df681322532da761f3bf14ea7ce7b399e
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2024-03-09 19:42:36 +0400

    [MSTMS-2] implemented constructor.

--- src/models/Message.php
+++ src/models/Message.php
@@ -6,4 +6,12 @@ class Message
 {
     private $text;
     private $title;
+
+    public function __construct(
+        string $text,
+        ?string $title = null
+    ) {
+        $this->text = $text;
+        $this->title = $title;
+    }
 }
