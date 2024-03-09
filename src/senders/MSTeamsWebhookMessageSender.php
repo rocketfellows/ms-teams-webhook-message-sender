dacdf61f0211f1b0c5ef02a10481c21d825a4700
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2024-03-09 21:58:44 +0400

    [MSTMS-2] injected client.

--- src/senders/MSTeamsWebhookMessageSender.php
+++ src/senders/MSTeamsWebhookMessageSender.php
@@ -2,6 +2,15 @@
 
 namespace rocketfellows\MSTeamsWebhookMessageSender\senders;
 
+use GuzzleHttp\Client;
+
 class MSTeamsWebhookMessageSender
 {
+    private $client;
+
+    public function __construct(
+        Client $client
+    ) {
+        $this->client = $client;
+    }
 }
