75c4a16da369a2dc940c06d6f5cb7e48ecfa3716
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2024-03-09 22:17:00 +0400

    [MSTMS-2] updated interface contract.

--- src/MSTeamsWebhookMessageSenderInterface.php
+++ src/MSTeamsWebhookMessageSenderInterface.php
@@ -2,6 +2,16 @@
 
 namespace rocketfellows\MSTeamsWebhookMessageSender;
 
+use rocketfellows\MSTeamsWebhookMessageSender\configs\Connector;
+use rocketfellows\MSTeamsWebhookMessageSender\exceptions\configs\EmptyIncomingWebhookUrlException;
+use rocketfellows\MSTeamsWebhookMessageSender\exceptions\configs\InvalidIncomingWebhookUrlException;
+use rocketfellows\MSTeamsWebhookMessageSender\models\Message;
+
 interface MSTeamsWebhookMessageSenderInterface
 {
+    /**
+     * @throws EmptyIncomingWebhookUrlException
+     * @throws InvalidIncomingWebhookUrlException
+     */
+    public function sendMessage(Connector $connector, Message $message): void;
 }
