c590644ec15a18cc306f74ba2f0cb9e75cc6485b
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2024-03-09 19:25:15 +0400

    [MSTMS-2] implemented constructor.

--- src/config/Connector.php
+++ src/config/Connector.php
@@ -5,4 +5,10 @@ namespace rocketfellows\MSTeamsWebhookMessageSender\config;
 class Connector
 {
     private $incomingWebhookUrl;
+
+    public function __construct(
+        string $incomingWebhookUrl
+    ) {
+        $this->incomingWebhookUrl = $incomingWebhookUrl;
+    }
 }
