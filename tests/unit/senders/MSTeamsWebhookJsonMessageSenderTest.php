66c77ce1d01b1ea02b40f6392c0126aef67aee45
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2024-03-11 20:17:09 +0400

    [MSTMS-2] setup test.

--- tests/unit/senders/MSTeamsWebhookJsonMessageSenderTest.php
+++ tests/unit/senders/MSTeamsWebhookJsonMessageSenderTest.php
@@ -2,11 +2,38 @@
 
 namespace rocketfellows\MSTeamsWebhookMessageSender\tests\unit\senders;
 
+use GuzzleHttp\Client;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
+use rocketfellows\MSTeamsWebhookMessageSender\senders\MSTeamsWebhookMessageSender;
 
 /**
  * @group ms-teams-webhook-message-senders
  */
 class MSTeamsWebhookJsonMessageSenderTest extends TestCase
 {
+    private const EXPECTED_IMPLEMENTED_INTERFACES = [
+        MSTeamsWebhookJsonMessageSenderInterface::class,
+    ];
+
+    /**
+     * @var MSTeamsWebhookJsonMessageSenderInterface
+     */
+    private $sender;
+
+    /**
+     * @var Client|MockObject
+     */
+    private $client;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->client = $this->createMock(Client::class);
+
+        $this->sender = new MSTeamsWebhookMessageSender(
+            $this->client
+        );
+    }
 }
