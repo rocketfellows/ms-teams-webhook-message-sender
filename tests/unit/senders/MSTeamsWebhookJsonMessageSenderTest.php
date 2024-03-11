979158d02fdf3bd7bd01dc1d75f2165e411c37c4
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2024-03-11 20:16:09 +0400

    [MSTMS-2] created ms teams webhook json message sender test.

---
+++ tests/unit/senders/MSTeamsWebhookJsonMessageSenderTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace rocketfellows\MSTeamsWebhookMessageSender\tests\unit\senders;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @group ms-teams-webhook-message-senders
+ */
+class MSTeamsWebhookJsonMessageSenderTest extends TestCase
+{
+}
