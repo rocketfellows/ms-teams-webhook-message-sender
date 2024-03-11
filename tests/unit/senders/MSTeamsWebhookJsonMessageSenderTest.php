63e9f1d2ea1a2bfa72778498ed37afe068459d9a
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2024-03-11 20:28:54 +0400

    [MSTMS-2] renamed.

--- tests/unit/senders/MSTeamsWebhookJsonMessageSenderTest.php
+++ tests/unit/senders/MSTeamsWebhookJsonMessageSenderTest.php
@@ -62,7 +62,7 @@ class MSTeamsWebhookJsonMessageSenderTest extends TestCase
     public function getInvalidJsonMessageProvidedData(): array
     {
         return [
-            'valid connector, empty json string' => [
+            'valid connector, json message is empty string' => [
                 'connector' => new Connector('https://foo.com/'),
                 'jsonMessage' => '',
                 'expectedExceptionClass' => InvalidJsonMessageException::class,
