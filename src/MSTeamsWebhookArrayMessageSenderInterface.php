9cfb821e5d30cb86b06815bc05e9fc4dbdf2b1d9
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2024-03-12 20:09:07 +0400

    [MSTMS-2] updated interface contract.

--- src/MSTeamsWebhookArrayMessageSenderInterface.php
+++ src/MSTeamsWebhookArrayMessageSenderInterface.php
@@ -2,6 +2,9 @@
 
 namespace rocketfellows\MSTeamsWebhookMessageSender;
 
+use rocketfellows\MSTeamsWebhookMessageSender\configs\Connector;
+
 interface MSTeamsWebhookArrayMessageSenderInterface
 {
+    public function sendMessageFromArray(Connector $connector, array $messageData): void;
 }
