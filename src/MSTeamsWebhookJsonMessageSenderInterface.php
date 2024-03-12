fe9019e778218d7533f3e20af911d5f9812ae544
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2024-03-12 19:36:48 +0400

    [MSTMS-2] updated interface contract.

--- src/MSTeamsWebhookJsonMessageSenderInterface.php
+++ src/MSTeamsWebhookJsonMessageSenderInterface.php
@@ -2,6 +2,9 @@
 
 namespace rocketfellows\MSTeamsWebhookMessageSender;
 
+use rocketfellows\MSTeamsWebhookMessageSender\configs\Connector;
+
 interface MSTeamsWebhookJsonMessageSenderInterface
 {
+    public function sendJsonMessage(Connector $connector, string $jsonMessage): void;
 }
